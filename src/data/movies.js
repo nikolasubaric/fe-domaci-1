d44ec87b89db3222ee1afb9e968f95362565c00b
nikolasubaric <nikolasubaric3@gmail.com>
2022-10-04 09:23:31 +0200

    Added movies array with data

---
+++ src/data/movies.js
@@ -0,0 +1,148 @@
+export const movies = [
+  {
+    name: 'Pulp Fiction',
+    year: 1994,
+    genre: 'Crime',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg',
+    actors: ['John Travolta', 'Uma Thurman', 'Samuel L. Jackson'],
+  },
+  {
+    name: 'About Time',
+    year: 2013,
+    genre: 'Drama',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BMTA1ODUzMDA3NzFeQTJeQWpwZ15BbWU3MDgxMTYxNTk@._V1_.jpg',
+    actors: ['John Travolta', 'Uma Thurman', 'Samuel L. Jackson'],
+  },
+  {
+    name: 'The Shawshank Redemption',
+    year: 1994,
+    genre: 'Drama',
+    image: 'https://flxt.tmsimg.com/assets/p15987_p_v8_ai.jpg',
+    actors: ['Tim Robbins', 'Morgan Freeman'],
+  },
+  {
+    name: 'Fight Club',
+    year: 1999,
+    genre: 'Drama',
+    image: 'https://lumiere-a.akamaihd.net/v1/images/image_174b2bb6.jpeg',
+    actors: ['Brad Pitt', 'Edward Norton'],
+  },
+  {
+    name: 'Inception',
+    year: 2010,
+    genre: 'Action',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg',
+    actors: [
+      'Leonardo DiCaprio',
+      'Joseph Gordon-Levitt',
+      'Ellen Page(Elliot Page)',
+    ],
+  },
+  {
+    name: `One Flew Over the Cuckoo's Nest`,
+    year: 1975,
+    genre: 'Drama',
+    image:
+      'https://static.wikia.nocookie.net/cinemorgue/images/e/e7/Large_srr59GKJdDXPwnWlew9NoYfOvYV.jpg/revision/latest/scale-to-width-down/470?cb=20161121074858',
+    actors: ['Jack Nicholson', 'Louise Fletcher'],
+  },
+  {
+    name: 'The Silence of the Lambs',
+    year: 1991,
+    genre: 'Crime',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BNjNhZTk0ZmEtNjJhMi00YzFlLWE1MmEtYzM1M2ZmMGMwMTU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg',
+    actors: ['Jodie Foster', 'Anthony Hopkins'],
+  },
+  {
+    name: 'Sinister',
+    year: 2012,
+    genre: 'Horror',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BMjI5MTg1Njg0Ml5BMl5BanBnXkFtZTcwNzg2Mjc4Nw@@._V1_.jpg',
+    actors: ['Ethan Hawke', 'Juliet Rylance'],
+  },
+  {
+    name: 'Insidious',
+    year: 2010,
+    genre: 'Horror',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BMTYyOTAxMDA0OF5BMl5BanBnXkFtZTcwNzgwNTc1NA@@._V1_FMjpg_UX1000_.jpg',
+    actors: ['Patrick Wilson', 'Rose Byrne', 'Lin Shaye'],
+  },
+  {
+    name: 'Untouchable',
+    year: 2011,
+    genre: 'Biography',
+    image: 'https://pics.filmaffinity.com/Untouchable-113587882-large.jpg',
+    actors: ['Francois Cluzet', 'Omar Sy'],
+  },
+  {
+    name: 'The Dark Knight Rises',
+    year: 2012,
+    genre: 'Action',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BMTk4ODQzNDY3Ml5BMl5BanBnXkFtZTcwODA0NTM4Nw@@._V1_FMjpg_UX1000_.jpg',
+    actors: ['Christian Bale', 'Tom Hardy'],
+  },
+  {
+    name: 'Avengers: Endgame',
+    year: 2019,
+    genre: 'Action',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg',
+    actors: ['Robert Downey Jr', 'Chris Evans', 'Mark Ruffalo'],
+  },
+  {
+    name: 'Good Will Hunting',
+    year: 1997,
+    genre: 'Drama',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BOTI0MzcxMTYtZDVkMy00NjY1LTgyMTYtZmUxN2M3NmQ2NWJhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg',
+    actors: ['Robin Williams', 'Matt Damon', 'Ben Affleck'],
+  },
+  {
+    name: 'The Hunt',
+    year: 2012,
+    genre: 'Drama',
+    image: 'https://ruthlessculture.files.wordpress.com/2014/04/th1.jpg',
+    actors: ['Mads Mikkelsen', 'Thomas Bo Larsen'],
+  },
+  {
+    name: 'Amelie',
+    year: 2001,
+    genre: 'Comedy',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BNDg4NjM1YjMtYmNhZC00MjM0LWFiZmYtNGY1YjA3MzZmODc5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_.jpg',
+    actors: ['Audrey Tautou', 'Mathieu Kassovitz'],
+  },
+  {
+    name: 'Shutter Island',
+    year: 2010,
+    genre: 'Mystery',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BYzhiNDkyNzktNTZmYS00ZTBkLTk2MDAtM2U0YjU1MzgxZjgzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg',
+    actors: ['Leonardo DiCaprio', 'Mark Ruffalo'],
+  },
+  {
+    name: 'Joker',
+    year: 2019,
+    genre: 'Crime',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg',
+    actors: ['Joaquin Phoenix', 'Robert De Niro'],
+  },
+  {
+    name: 'The Departed',
+    year: 2006,
+    genre: 'Crime',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BMTI1MTY2OTIxNV5BMl5BanBnXkFtZTYwNjQ4NjY3._V1_FMjpg_UX1000_.jpg',
+    actors: ['Leonardo DiCaprio', 'Matt Damon', 'Jack Nicholson'],
+  },
+];
+
+export default movies;
