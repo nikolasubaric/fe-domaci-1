5c0bf70e1a6ec29e050192cd9cb9f6998935b289
nikolasubaric <nikolasubaric3@gmail.com>
2022-10-06 09:50:24 +0200

    Switched useReducer for useState

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -4,36 +4,19 @@ import FindMovie from '../FIndMovie/FindMovie';
 import Container from '../Container/Container';
 
 import classes from './Movies.module.css';
-import { useReducer, Fragment } from 'react';
-
-const pageReducer = (state, action) => {
-  if (action.type === 'LIST') {
-    return { onMoviesList: true, onStatistics: false, onFindMovie: false };
-  }
-  if (action.type === 'STATS') {
-    return { onMoviesList: false, onStatistics: true, onFindMovie: false };
-  }
-  if (action.type === 'SEARCH') {
-    return { onMoviesList: false, onStatistics: false, onFindMovie: true };
-  }
-  return { onMoviesList: true, onStatistics: false, onFindMovie: false };
-};
+import { Fragment, useState } from 'react';
 
 const Movies = () => {
-  const [pageState, dispatchState] = useReducer(pageReducer, {
-    onMoviesList: true,
-    onStatistics: false,
-    onFindMovie: false,
-  });
+  const [page, setPage] = useState('list');
 
   const listHandler = () => {
-    dispatchState({ type: 'LIST' });
+    setPage('list');
   };
   const statsHandler = () => {
-    dispatchState({ type: 'STATS' });
+    setPage('stats');
   };
   const searchHandler = () => {
-    dispatchState({ type: 'SEARCH' });
+    setPage('search');
   };
 
   return (
@@ -41,23 +24,21 @@ const Movies = () => {
       <div className={classes.header}>
         <button
           className={`${classes.pageBtn} ${
-            pageState.onMoviesList ? classes.active : ''
+            page === 'list' ? classes.active : ''
           }`}
           onClick={listHandler}
         >
           LIST
         </button>
         <button
-          className={`${classes.pageBtn} ${
-            pageState.onStatistics && classes.active
-          }`}
+          className={`${classes.pageBtn} ${page === 'stats' && classes.active}`}
           onClick={statsHandler}
         >
           STATISTICS
         </button>
         <button
           className={`${classes.pageBtn} ${
-            pageState.onFindMovie && classes.active
+            page === 'search' && classes.active
           }`}
           onClick={searchHandler}
         >
@@ -65,9 +46,9 @@ const Movies = () => {
         </button>
       </div>
       <Container>
-        {pageState.onMoviesList && <MoviesList />}
-        {pageState.onStatistics && <MoviesStatistics />}
-        {pageState.onFindMovie && <FindMovie />}
+        {page === 'list' && <MoviesList />}
+        {page === 'stats' && <MoviesStatistics />}
+        {page === 'search' && <FindMovie />}
       </Container>
     </Fragment>
   );
